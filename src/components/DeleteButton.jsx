1477ad8a6014fc7ca3e182a2d07f1b6951b299d5
Antton Kasslin <antton.kasslin@hotmail.com>
2024-05-22 21:56:38 +0300

    5.4 AND did some preliminary checks in the delete department

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,18 +1,29 @@
 import { useState } from "react"
 import blogService from '../services/blogs'
 
+blogService.setToken(user.token)
+
 const DeleteButton = ({ blog, blogs, setBlogs, setSuccessMessage, setErrorMessage}) => {
   
     const handleDeleteButtonClick = () => {     
         console.log("blog.title that handleDeleteButtonClick is referring to:", blog.title)
         try {
-            const response = blogService.remove(blog.id) // removal from the database!
-            setBlogs(blogs.filter(aBlog => aBlog.id !== blog.id))
-            console.log("deleted! status:", response.status)
-            setSuccessMessage(`blog ("${blog.title}") deleted`)
-            setTimeout(() => {        
-                setSuccessMessage(null)  // = show the error message for 5 seconds, then set the error message to null again    
-            }, 5000)
+            const response = blogService.remove(blog.id).then(response => {
+                console.log("status:", response.status)
+                if (response.status === 204) {
+                    setBlogs(blogs.filter(aBlog => aBlog.id !== blog.id))
+                    setSuccessMessage(`blog ("${blog.title}") deleted`)
+                    setTimeout(() => {        
+                            setSuccessMessage(null)  // = show the error message for 5 seconds, then set the error message to null again    
+                        }, 5000)
+                } else {
+                    setErrorMessage(`it is possible only to remove blogs you have added yourself`)
+                    setTimeout(() => {        
+                        setSuccessMessage(null)  // = show the error message for 5 seconds, then set the error message to null again    
+                    }, 5000)
+                }
+            }) // removal from the database!
+            
         } catch {
             (error => console.error("Error in DeleteButton:", error))
             setErrorMessage(`it is possible only to remove blogs you have added yourself`)
